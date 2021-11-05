a7440b964bbc1f2c2cbd5f3fd1df55642618c6ea
Martin <martinpfatrisch@gmail.com>
2021-11-05 10:28:20 +0100

    Finished the navigation markup

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,6 +7,18 @@
         <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
         <h1 class="text-lg">Active Tracker</h1>
       </div>
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }"
+          >Home</router-link
+        >
+        <router-link class="cursor-pointer" :to="{ name: '' }"
+          >Create</router-link
+        >
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }"
+          >Login</router-link
+        >
+        <li class="cursor-pointer">Logout</li>
+      </ul>
     </nav>
   </header>
 </template>
