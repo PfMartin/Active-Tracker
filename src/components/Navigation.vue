fc58857091f5e4a2a3a16087608c5afde8ec6f7b
Martin <martinpfatrisch@gmail.com>
2021-11-05 10:20:53 +0100

    Setup of the Navigation Header and Navigation icon

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,14 @@
 <template>
-  <header></header>
+  <header class="bg-at-light-green text-white">
+    <nav
+      class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row"
+    >
+      <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
+        <h1 class="text-lg">Active Tracker</h1>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -12,6 +21,6 @@ export default {
     // Logout function
 
     return {};
-  },
+  }
 };
 </script>
