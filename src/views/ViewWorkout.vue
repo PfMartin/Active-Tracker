ecd2a948cb5ddbc5c42b9345f83b934679539196
Martin <martinpfatrisch@gmail.com>
2021-11-05 14:36:17 +0100

    Set Route to viewing a single workout and implemented functionality to fetch the data

--- src/views/ViewWorkout.vue
+++ src/views/ViewWorkout.vue
@@ -3,14 +3,43 @@
 </template>
 
 <script>
+import { ref } from "vue";
+import { supabase } from "../supabase/init";
+import { useRoute } from "vue-router";
+
 export default {
   name: "view-workout",
   setup() {
     // Create data / vars
+    const data = ref(null);
+    const dataLoaded = ref(null);
+    const errorMsg = ref(null);
+    const statusMsg = ref(null);
+    const route = useRoute();
 
     // Get current Id of route
+    const currentId = route.params.workoutId;
 
     // Get workout data
+    const getData = async () => {
+      try {
+        const { data: workouts, error } = await supabase
+          .from("workouts")
+          .select("*")
+          .eq("id", currentId);
+        data.value = workouts;
+        dataLoaded.value = true;
+        console.log(data.value);
+        if (error) throw error;
+      } catch (error) {
+        errorMsg.value = error.message;
+        setTimeout(() => {
+          errorMsg.value = null;
+        }, 5000);
+      }
+    };
+
+    getData();
 
     // Delete workout
 
@@ -22,7 +51,7 @@ export default {
 
     // Update Workout
 
-    return {};
-  },
+    return { data, dataLoaded, errorMsg, statusMsg };
+  }
 };
 </script>
