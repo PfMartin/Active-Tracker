0f4c54a7a33cb5dbe2927460e962f46417fe2f6a
Martin <martinpfatrisch@gmail.com>
2021-11-05 11:41:06 +0100

    Added conditional rendering for the navigation links

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,25 +11,28 @@
         <router-link class="cursor-pointer" :to="{ name: 'Home' }"
           >Home</router-link
         >
-        <router-link class="cursor-pointer" :to="{ name: '' }"
+        <router-link v-if="user" class="cursor-pointer" :to="{ name: '' }"
           >Create</router-link
         >
-        <router-link class="cursor-pointer" :to="{ name: 'Login' }"
+        <router-link v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }"
           >Login</router-link
         >
-        <li @click="logout" class="cursor-pointer">Logout</li>
+        <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
       </ul>
     </nav>
   </header>
 </template>
 
 <script>
+import store from "../store/index";
+import { computed } from "vue";
 import { supabase } from "../supabase/init";
 import { useRouter } from "vue-router";
 
 export default {
   setup() {
     // Get user from store
+    const user = computed(() => store.state.user);
 
     // Setup ref to router
     const router = useRouter();
@@ -42,7 +45,7 @@ export default {
       });
     };
 
-    return { logout };
+    return { logout, user };
   }
 };
 </script>
